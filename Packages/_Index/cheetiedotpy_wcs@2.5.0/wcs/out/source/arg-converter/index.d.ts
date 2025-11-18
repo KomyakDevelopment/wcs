4171af68d3145baa611e4045ad43c3d28881a673
Komyak <dynasoftstudios@gmail.com>
2025-11-18 22:55:36 +0100

    Remove package folders and lock file from tracking

--- Packages/_Index/cheetiedotpy_wcs@2.5.0/wcs/out/source/arg-converter/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function ConvertArgs(args: unknown[]): Map<number, unknown>;
-export declare function RestoreArgs(args: Map<number, unknown>): unknown[];
--- Packages/wcs.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent._Index["cheetiedotpy_wcs@2.5.0"]["wcs"])
